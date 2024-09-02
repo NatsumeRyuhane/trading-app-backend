360e6aa782a84a1e633e2c0f361bd6a92f832ce6
Steven Yi <stevenjxhc@gmail.com>
2024-09-02 01:59:11 -0500

    refactor rating logic into service and update RatingResponse

--- src/main/java/com/flag3/tradingappbackend/user/RatingResponse.java
+++ src/main/java/com/flag3/tradingappbackend/user/RatingResponse.java
@@ -1,19 +1,6 @@
 package com.flag3.tradingappbackend.user;
 
-public class RatingResponse {
-    private double totolRating;
-    private int RatingUserSize;
-
-    public RatingResponse(double totolRating, int RatingUserSize) {
-        this.totolRating = totolRating;
-        this.RatingUserSize = RatingUserSize;
-    }
-
-    public double getTotolRating() {
-        return totolRating;
-    }
-
-    public int getRatingUserSize() {
-        return RatingUserSize;
-    }
-}
+public record RatingResponse(
+        double rating,
+        int numRatings
+) {}
--- src/main/java/com/flag3/tradingappbackend/user/UserController.java
+++ src/main/java/com/flag3/tradingappbackend/user/UserController.java
@@ -10,34 +10,24 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/auth")
 @AllArgsConstructor
 public class UserController {
     private final TransactionService transactionService;
     private final UserService userService;
 
-    @PostMapping("/register")
+    @PostMapping("/auth/register")
     public void register(@RequestBody RegisterRequest body) {
         userService.register(body.username(), body.password(), body.firstName(), body.lastName(), body.address());
     }
 
-    @PostMapping("/login")
+    @PostMapping("/auth/login")
     public LoginResponse login(@RequestBody LoginRequest body) {
         String token = userService.login(body.username(), body.password());
         return new LoginResponse(token);
     }
 
-    @PostMapping("/get/users/rating/{id}")
-    public RatingResponse getRating(@PathVariable UUID id) {
-        List<TransactionDto> transactionList = transactionService.getTransactionsByBuyerId(id);
-        double total = 0.0;
-        int size = 0;
-        for (TransactionDto i : transactionList) {
-            if (i.buyerToSellerRating() != null) {
-                total += i.buyerToSellerRating();
-                size++;
-            }
-        }
-        return new RatingResponse(total, size);
+    @GetMapping("/users/rating")
+    public RatingResponse getRating(@RequestParam UUID id) {
+        return userService.getUserRating(id);
     }
 }
